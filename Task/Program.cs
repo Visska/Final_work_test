fae8e27a85708e69731731e1d46c3f59b017bbb8
Visska <strannica.ev@gmail.com>
2022-08-05 02:28:07 +0300

    add Task

---
+++ Task/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+Console.WriteLine("Имеющийся массив строк: ");
+string[] arrayString = {"hello", "2", "world", ":-)"};
+printArray(arrayString);
+int resultArrayLegth = lengthNewArray(arrayString);
+string[] resultArray = new string[resultArrayLegth];
+resultArray = createArrayLengthThree(arrayString, resultArray);
+Console.WriteLine("\nПолучившийся массив строк: ");
+printArray(resultArray);
+
+
+int lengthNewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+string[] createArrayLengthThree(string[] array, string[] array2)
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[index] = array[i];
+            index++;
+        }
+    }
+    return array2;
+}
+
+
+void printArray(string[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if(i < array.Length-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
